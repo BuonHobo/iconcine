94801edcbb2089773b90a8dca168cbd32af0e3cc
buonhobo <naturytropik@gmail.com>
2022-06-08 21:58:13 +0200

    Supporto a logging e timer per la prossima icona

--- iconcina.py
+++ iconcina.py
@@ -1,29 +1,46 @@
-from lib2to3.pgen2 import token
+import asyncio
 import discord
 from discord.ext import tasks
 from pathlib import Path
 from random import choice
+import datetime
+import time
 
-immagini=[immagine for immagine in Path.cwd().joinpath("immagini").iterdir()]
-with open("token.txt","r") as file:
-    tkn=file.readline().strip()
+immagini = [immagine for immagine in Path.cwd().joinpath("immagini").iterdir()]
+with open("token.txt", "r") as file:
+    tkn = file.readline().strip()
+
 
 class MyClient(discord.Client):
     async def on_ready(self):
-        slow_count.start(self)
-        print("Ready to go")
-        
+        update_icon.start(self)
+        update_counter.start(self)
+        print("Ready to go\n")
+
+
+@tasks.loop(seconds=10)
+async def update_counter(bot: MyClient):
+    seconds:datetime.timedelta=(update_icon.next_iteration-datetime.datetime.now(datetime.timezone.utc)).seconds/60
+    seconds=round(seconds)
+    activity= discord.Game(f"Next in {seconds}")
+    await bot.change_presence(activity=activity)
+
 
 @tasks.loop(minutes=60)
-async def slow_count(bot):
+async def update_icon(bot):
     global immagini
     scelta = choice(immagini)
-    print(f'I\'m about to change server icon to {scelta}')
-    guild:discord.Guild=bot.get_guild(703342439028490341)
+    guild: discord.Guild = bot.get_guild(703342439028490341)
+
     with scelta.open("rb") as immagine:
-        icon=immagine.read()
-    await guild.edit(reason="Cambio periodico dell'immagine",icon=icon)
-    print("done")
+        icon = immagine.read()
+
+    start = time.time()
+    await guild.edit(reason="Cambio periodico dell'immagine", icon=icon)
+    end = time.time()
+
+    with open("log.csv","a") as file:
+        file.write(f"{datetime.datetime.now()},{scelta.name},{round(end-start,2)}")
 
 
 intents = discord.Intents.default()
